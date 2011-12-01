640b136f261600f10a6fcbd67913201d65e5c0ec
Stefan Thomas <justmoon@members.fsf.org>
2011-12-01 06:50:59 +0100

    Tabs -> spaces.

--- app.js
+++ app.js
@@ -23,39 +23,39 @@ var storage = new bitcoin.Storage(config.storage.uri);
 // Configuration
 
 app.configure(function(){
-	app.set('views', __dirname + '/views');
-	app.set('view engine', 'ejs');
-	app.use(express.bodyParser());
-	app.use(express.methodOverride());
-	app.use(app.router);
-	app.use(express.static(__dirname + '/public'));
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
-	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 app.configure('production', function(){
-	app.use(express.errorHandler());
+  app.use(express.errorHandler());
 });
 
 function getOutpoints(txs, callback) {
-	// If we got only one tx, wrap it so we can use the same code afterwards
-	if (txs.hash) txs = [txs];
+  // If we got only one tx, wrap it so we can use the same code afterwards
+  if (txs.hash) txs = [txs];
 
-	var txList = {};
-	txs.forEach(function (tx) {
-		tx.ins.forEach(function (txin) {
+  var txList = {};
+  txs.forEach(function (tx) {
+    tx.ins.forEach(function (txin) {
       if (Util.NULL_HASH.equals(txin.outpoint.hash)) {
         return;
       }
-			txList[txin.o.toString('base64')] = {
+      txList[txin.o.toString('base64')] = {
         hash: txin.outpoint.hash,
         index: txin.outpoint.index,
         target: txin
       };
-		});
-	});
+    });
+  });
 
   Step(function () {
     var parallel = this.parallel;
@@ -80,19 +80,19 @@ function getOutpoints(txs, callback) {
       });
     });
   }, function (err) {
-		txs.forEach(function (tx, i) {
-			tx.totalIn = bigint(0);
-			tx.totalOut = bigint(0);
-			tx.ins.forEach(function (txin, j) {
+    txs.forEach(function (tx, i) {
+      tx.totalIn = bigint(0);
+      tx.totalOut = bigint(0);
+      tx.ins.forEach(function (txin, j) {
         if (txin.isCoinBase()) return;
 
-				tx.totalIn = tx.totalIn.add(Util.valueToBigInt(txin.source.value));
-			});
-			tx.outs.forEach(function (txout) {
-				tx.totalOut = tx.totalOut.add(Util.valueToBigInt(txout.value));
-			});
-			if (!tx.isCoinBase()) tx.fee = tx.totalIn.sub(tx.totalOut);
-		});
+        tx.totalIn = tx.totalIn.add(Util.valueToBigInt(txin.source.value));
+      });
+      tx.outs.forEach(function (txout) {
+        tx.totalOut = tx.totalOut.add(Util.valueToBigInt(txout.value));
+      });
+      if (!tx.isCoinBase()) tx.fee = tx.totalIn.sub(tx.totalOut);
+    });
 
     this();
   }, callback);
@@ -101,15 +101,15 @@ function getOutpoints(txs, callback) {
 // Params
 
 app.param('blockHash', function (req, res, next, hash){
-	hash = Util.decodeHex(hash).reverse();
-	storage.Block.findOne({_id: hash}, function (err, block) {
-		if (err) return next(err);
+  hash = Util.decodeHex(hash).reverse();
+  storage.Block.findOne({_id: hash}, function (err, block) {
+    if (err) return next(err);
 
-		storage.Block.findOne({prev_hash: hash}, function (err, nextBlock) {
-			if (err) return next(err);
+    storage.Block.findOne({prev_hash: hash}, function (err, nextBlock) {
+      if (err) return next(err);
 
-			storage.Transaction.find({_id: {$in: block.txs}}, function (err, txs) {
-				if (err) return next(err);
+      storage.Transaction.find({_id: {$in: block.txs}}, function (err, txs) {
+        if (err) return next(err);
 
         // Order the transactions
         var txIndex = {};
@@ -126,224 +126,224 @@ app.param('blockHash', function (req, res, next, hash){
           txs.push(tx);
         });
 
-				getOutpoints(txs, function (err) {
-					if (err) return next(err);
+        getOutpoints(txs, function (err) {
+          if (err) return next(err);
 
-					var totalFee = bigint(0);
-					var totalOut = bigint(0);
-					txs.forEach(function (tx) {
-						tx.outs.forEach(function (txout) {
-							totalOut = totalOut.add(Util.valueToBigInt(txout.value));
-						});
-						if (tx.fee) totalFee = totalFee.add(tx.fee);
-					});
+          var totalFee = bigint(0);
+          var totalOut = bigint(0);
+          txs.forEach(function (tx) {
+            tx.outs.forEach(function (txout) {
+              totalOut = totalOut.add(Util.valueToBigInt(txout.value));
+            });
+            if (tx.fee) totalFee = totalFee.add(tx.fee);
+          });
 
-					req.block = block;
-					req.nextBlock = nextBlock;
-					req.txs = txs;
-					req.totalFee = totalFee;
-					req.totalOut = totalOut;
+          req.block = block;
+          req.nextBlock = nextBlock;
+          req.txs = txs;
+          req.totalFee = totalFee;
+          req.totalOut = totalOut;
 
-					next();
-				});
-			});
-		});
-	});
+          next();
+        });
+      });
+    });
+  });
 });
 
 app.param('txHash', function (req, res, next, hash){
-	hash = Util.decodeHex(hash).reverse();
-	storage.Transaction.findOne({_id: hash}, function (err, tx) {
-		if (err) return next(err);
-		req.tx = tx;
+  hash = Util.decodeHex(hash).reverse();
+  storage.Transaction.findOne({_id: hash}, function (err, tx) {
+    if (err) return next(err);
+    req.tx = tx;
 
     // TODO: Show side chain blocks containing this tx
-		storage.Block.findOne({txs: tx._id, active: true}, function (err, block) {
-			if (err) return next(err);
-			req.block = block;
+    storage.Block.findOne({txs: tx._id, active: true}, function (err, block) {
+      if (err) return next(err);
+      req.block = block;
 
-			getOutpoints(tx, function (err) {
-				if (err) return next(err);
+      getOutpoints(tx, function (err) {
+        if (err) return next(err);
 
-				next();
-			});
-		});
-	});
+        next();
+      });
+    });
+  });
 });
 
 app.param('addrBase58', function (req, res, next, addr){
-	var pubKeyHash = Util.addressToPubKeyHash(addr);
-	req.pubKeyHash = pubKeyHash;
+  var pubKeyHash = Util.addressToPubKeyHash(addr);
+  req.pubKeyHash = pubKeyHash;
 
   // TODO: We have to limit no of transactions. Need to implement paging and fix
   //       "spent in" for this case.
-	storage.Transaction.find({affects: pubKeyHash}).limit(100).exec(function (err, txs) {
-		if (err) return next(err);
+  storage.Transaction.find({affects: pubKeyHash}).limit(100).exec(function (err, txs) {
+    if (err) return next(err);
 
-		var txList = txs.map(function (tx) {
-			return tx._id;
-		});
+    var txList = txs.map(function (tx) {
+      return tx._id;
+    });
 
-		storage.Block.find({txs: {$in: txList}}, function (err, blocks) {
-			if (err) return next(err);
+    storage.Block.find({txs: {$in: txList}}, function (err, blocks) {
+      if (err) return next(err);
 
-			getOutpoints(txs, function (err) {
-				if (err) return next(err);
+      getOutpoints(txs, function (err) {
+        if (err) return next(err);
 
-				var txsObj = {};
-				txs.forEach(function (tx) {
-					txsObj[tx.hash.toString('base64')] = tx;
-				});
-				blocks.forEach(function (block) {
+        var txsObj = {};
+        txs.forEach(function (tx) {
+          txsObj[tx.hash.toString('base64')] = tx;
+        });
+        blocks.forEach(function (block) {
           block.txs.forEach(function (tx) {
             var hash64 = tx.toString('base64');
             if (txsObj[hash64]) {
               txsObj[hash64].blockObj = block;
             }
           });
-				});
-				req.txsObj = txsObj;
+        });
+        req.txsObj = txsObj;
 
-				var receivedCount = 0;
-				var receivedAmount = bigint(0);
-				var sentCount = 0;
-				var sentAmount = bigint(0);
+        var receivedCount = 0;
+        var receivedAmount = bigint(0);
+        var sentCount = 0;
+        var sentAmount = bigint(0);
 
-				var txOutsObj = {};
-				txs.forEach(function (tx, index) {
-					for (var i = 0; i < tx.outs.length; i++) {
-						var txout = tx.outs[i];
-						var script = txout.getScript();
+        var txOutsObj = {};
+        txs.forEach(function (tx, index) {
+          for (var i = 0; i < tx.outs.length; i++) {
+            var txout = tx.outs[i];
+            var script = txout.getScript();
 
-						var outPubKey = script.simpleOutPubKeyHash();
+            var outPubKey = script.simpleOutPubKeyHash();
 
-						if (outPubKey && pubKeyHash.compare(outPubKey) == 0) {
-							receivedCount++;
-							var outIndex =
-								tx.hash.toString('base64')+":"+
-								i;
-							txOutsObj[outIndex] = txout;
+            if (outPubKey && pubKeyHash.compare(outPubKey) == 0) {
+              receivedCount++;
+              var outIndex =
+                tx.hash.toString('base64')+":"+
+                i;
+              txOutsObj[outIndex] = txout;
 
-							receivedAmount = receivedAmount.add(Util.valueToBigInt(txout.value));
+              receivedAmount = receivedAmount.add(Util.valueToBigInt(txout.value));
 
-							tx.myOut = txout;
-						}
-					};
-				});
+              tx.myOut = txout;
+            }
+          };
+        });
 
-				txs.forEach(function (tx, index) {
-					if (tx.isCoinBase()) return;
+        txs.forEach(function (tx, index) {
+          if (tx.isCoinBase()) return;
 
-					tx.ins.forEach(function (txin, j) {
-						var script = txin.source.getScript();
+          tx.ins.forEach(function (txin, j) {
+            var script = txin.source.getScript();
 
-						var outPubKey = script.simpleOutPubKeyHash();
+            var outPubKey = script.simpleOutPubKeyHash();
 
-						if (outPubKey && pubKeyHash.compare(outPubKey) == 0) {
-							sentCount++;
-							var outIndex =
-								txin.outpoint.hash.toString('base64')+":"+
-								txin.outpoint.index;
+            if (outPubKey && pubKeyHash.compare(outPubKey) == 0) {
+              sentCount++;
+              var outIndex =
+                txin.outpoint.hash.toString('base64')+":"+
+                txin.outpoint.index;
 
-							if (!txOutsObj[outIndex]) {
-								winston.warn('Outgoing transaction is missing matching incoming transaction.');
-								return;
-							}
-							txOutsObj[outIndex].spent = {
-								txin: txin,
-								tx: tx
-							};
+              if (!txOutsObj[outIndex]) {
+                winston.warn('Outgoing transaction is missing matching incoming transaction.');
+                return;
+              }
+              txOutsObj[outIndex].spent = {
+                txin: txin,
+                tx: tx
+              };
 
-							sentAmount = sentAmount.add(Util.valueToBigInt(txin.source.value));
+              sentAmount = sentAmount.add(Util.valueToBigInt(txin.source.value));
 
-							tx.myIn = txin;
-						}
-					});
-				});
+              tx.myIn = txin;
+            }
+          });
+        });
 
         // Make sure the transactions actually have something to do with us
         txs = txs.filter(function (tx) {
           return tx.myOut || tx.myIn;
         });
 
-				// Calculate the current available balance
-				var totalAvailable = bigint(0);
-				for (var i in txOutsObj) {
-					if (!txOutsObj[i].spent) {
-						totalAvailable = totalAvailable.add(Util.valueToBigInt(txOutsObj[i].value));
-					}
-				}
+        // Calculate the current available balance
+        var totalAvailable = bigint(0);
+        for (var i in txOutsObj) {
+          if (!txOutsObj[i].spent) {
+            totalAvailable = totalAvailable.add(Util.valueToBigInt(txOutsObj[i].value));
+          }
+        }
 
         var account = {};
         account.pubKeyHash = pubKeyHash;
-				account.totalAvailable = totalAvailable;
-				account.receivedCount = receivedCount;
-				account.receivedAmount = receivedAmount;
-				account.sentCount = sentCount;
-				account.sentAmount = sentAmount;
+        account.totalAvailable = totalAvailable;
+        account.receivedCount = receivedCount;
+        account.receivedAmount = receivedAmount;
+        account.sentCount = sentCount;
+        account.sentAmount = sentAmount;
 
-				req.account = account;
-				req.txs = txs;
-				req.txOutsObj = txOutsObj;
+        req.account = account;
+        req.txs = txs;
+        req.txOutsObj = txOutsObj;
 
-				next();
-			});
-		});
-	});
+        next();
+      });
+    });
+  });
 });
 
 // Routes
 
 app.get('/', function(req, res){
-	storage.Block.find().sort('height', -1).limit(15).exec(function (err, rows) {
-		if (err) return next(err);
-		res.render('index', {
-			title: 'Home - Bitcoin Explorer',
-			latestBlocks: rows
-		});
-	});
+  storage.Block.find().sort('height', -1).limit(15).exec(function (err, rows) {
+    if (err) return next(err);
+    res.render('index', {
+      title: 'Home - Bitcoin Explorer',
+      latestBlocks: rows
+    });
+  });
 });
 
 app.get('/block/:blockHash', function (req, res) {
-	res.render('block', {
-		title: 'Block '+req.block.height+' - Bitcoin Explorer',
-		block: req.block,
+  res.render('block', {
+    title: 'Block '+req.block.height+' - Bitcoin Explorer',
+    block: req.block,
     txs: req.txs,
-		nextBlock: req.nextBlock,
-		totalAmount: req.totalAmount,
-		totalFee: req.totalFee,
-		totalOut: req.totalOut,
-		hexDifficulty: bigint(req.block.bits).toString(16)
-	});
+    nextBlock: req.nextBlock,
+    totalAmount: req.totalAmount,
+    totalFee: req.totalFee,
+    totalOut: req.totalOut,
+    hexDifficulty: bigint(req.block.bits).toString(16)
+  });
 });
 
 app.get('/tx/:txHash', function (req, res) {
-	var totalOut = bigint(0);
-	req.tx.outs.forEach(function (txout) {
-		totalOut = totalOut.add(Util.valueToBigInt(txout.value));
-	});
-	res.render('transaction', {
-		title: 'Tx '+Util.formatHashAlt(req.tx.hash)+'... - Bitcoin Explorer',
-		tx: req.tx,
-		block: req.block,
-		totalOut: totalOut
-	});
+  var totalOut = bigint(0);
+  req.tx.outs.forEach(function (txout) {
+    totalOut = totalOut.add(Util.valueToBigInt(txout.value));
+  });
+  res.render('transaction', {
+    title: 'Tx '+Util.formatHashAlt(req.tx.hash)+'... - Bitcoin Explorer',
+    tx: req.tx,
+    block: req.block,
+    totalOut: totalOut
+  });
 });
 
 app.get('/address/:addrBase58', function (req, res) {
-	res.render('address', {
-		title: 'Address '+(req.params.addrBase58)+' - Bitcoin Explorer',
-		address: req.params.addrBase58,
-		pubKeyHash: req.pubKeyHash,
-		account: req.account,
-		txs: req.txs,
-		txOutsObj: req.txOutsObj
-	});
+  res.render('address', {
+    title: 'Address '+(req.params.addrBase58)+' - Bitcoin Explorer',
+    address: req.params.addrBase58,
+    pubKeyHash: req.pubKeyHash,
+    account: req.account,
+    txs: req.txs,
+    txOutsObj: req.txOutsObj
+  });
 });
 
 // Only listen on $ node app.js
 
 if (!module.parent) {
-	app.listen(3000);
-	winston.info("Express server listening on port " + app.address().port);
+  app.listen(3000);
+  winston.info("Express server listening on port " + app.address().port);
 }
