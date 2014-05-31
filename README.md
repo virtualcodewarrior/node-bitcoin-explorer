be4e3a14a5920b0c8743dd49dd51c7d1d9e175fa
virtualcodewarrior <virtualcodewarrior@hotmail.com>
2014-05-30 20:19:34 -0400

    update documentation and fix things that got broken with versions of express >= 4.0

--- README.md
+++ README.md
@@ -1,32 +1,31 @@
 # node-bitcoin-explorer
 
 This is mostly intended as a demo application for
-[bitcoinjs-server](https://github.com/bitcoinjs/bitcoinjs-server). It
+[bitcoinjs-server](https://github.com/virtualcodewarrior/bitcoinjs-server). It
 replicates most of the functionality of the popular [Block
 Explorer](http://blockexplorer.com/) website.
 
 # Installation
 
 Before you install `node-bitcoin-explorer`, make sure you have
-[bitcoinjs-server](https://github.com/bitcoinjs/bitcoinjs-server)
+[bitcoinjs-server](https://github.com/virtualcodewarrior/bitcoinjs-server)
 installed.
 
 ``` sh
 # Get node-bitcoin-explorer
-git clone git://github.com/justmoon/node-bitcoin-explorer.git --recursive
+git clone git://github.com/virtualcodewarrior/node-bitcoin-explorer.git
 
 # Go to folder
 cd node-bitcoin-explorer
 
-# Link to global bitcoinjs installation
-npm link bitcoinjs
+# Link to the git version of bitcoinjs-server
+sudo npm link ../bitcoinjs-server
 
 # Install dependencies
 npm install
 ```
 
 # Usage
-
 To start the server
 
     node app.js
--- app.js
+++ app.js
@@ -1,147 +1,169 @@
-
-/**
- * Module dependencies.
- */
-
-var express = require('express');
-var winston = require('winston');
-var Step = require('step');
-var bitcoin = require('bitcoinjs');
-var RpcClient = require('jsonrpc2').Client;
-var bigint = global.bigint = bitcoin.bigint;
-
+// Module dependencies.
 global.Util = require('./util');
-var fs = require('fs');
 
-var init = require('bitcoinjs/daemon/init');
-var config = init.getConfig();
+var express = require('express'),
+	bodyParser = require('body-parser'),
+	methodOverride = require('method-override'),
+	errorHandler = require('errorhandler'),
+	http = require('http'),
+	winston = require('winston'),
+	Step = require('step'),
+	bitcoin = require('bitcoinjs'),
+	RpcClient = require('jsonrpc2').Client,
+	bigint = global.bigint = bitcoin.bigint,
 
-var app = module.exports = express.createServer();
+	fs = require('fs'),
+	init = require('bitcoinjs/daemon/init'),
+	
+	config = init.getConfig(),
 
-var rpcClient = new RpcClient(config.jsonrpc.port, config.jsonrpc.host,
-                              config.jsonrpc.username, config.jsonrpc.password);
+	app = express(),
+	server = http.createServer(app),
 
-var rpc = rpcClient.connectSocket();
+	rpcClient = new RpcClient(config.jsonrpc.port, config.jsonrpc.host, config.jsonrpc.username, config.jsonrpc.password),
 
-rpc.on('connect', function () {
-  var moduleSrc = fs.readFileSync(__dirname + '/query.js', 'utf8');
-  rpc.call('definerpcmodule', ['explorer', moduleSrc], function (err) {
-    if (err) {
-      console.error('Error registering query module: '+err.toString());
-    }
-  });
+	rpc = rpcClient.connectSocket();
+
+rpc.on('connect', function() 
+{
+	var moduleSrc = fs.readFileSync(__dirname + '/query.js', 'utf8');
+	rpc.call('definerpcmodule', ['explorer', moduleSrc], function (err) 
+	{
+		if (err) 
+		{
+			console.error('Error registering query module: '+err.toString());
+		}
+	});
 });
 
 // Configuration
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(bodyParser());
+app.use(methodOverride());
+app.use(express.static(__dirname + '/public'));
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
+var env = process.env.NODE_ENV || 'development';
+if ('development' == env)
+{
+	app.use(errorHandler({ dumpExceptions: true, showStack: true }));
+}
+else if ('production' == env)
+{
+	app.use(errorHandler());
+}
 
 // Params
+app.param('blockHash', function (req, res, next, hash)
+{
+	hash = Util.decodeHex(hash).reverse();
+	var hash64 = hash.toString('base64');
+	rpc.call('explorer.blockquery', [hash64], function (err, data) 
+	{
+		if (err) 
+		{
+			return next(err);
+		}
 
-app.param('blockHash', function (req, res, next, hash){
-  hash = Util.decodeHex(hash).reverse();
-  var hash64 = hash.toString('base64');
-  rpc.call('explorer.blockquery', [hash64], function (err, data) {
-    if (err) return next(err);
+		req.block = data;
 
-    req.block = data;
-
-    next();
-  });
+		next();
+	});
 });
 
-app.param('txHash', function (req, res, next, hash){
-  hash = Util.decodeHex(hash).reverse();
-  var hash64 = hash.toString('base64');
-  rpc.call('explorer.txquery', [hash64], function (err, tx) {
-    if (err) return next(err);
+app.param('txHash', function (req, res, next, hash)
+{
+	hash = Util.decodeHex(hash).reverse();
+	var hash64 = hash.toString('base64');
+	rpc.call('explorer.txquery', [hash64], function (err, tx) 
+	{
+		if (err) 
+		{
+			return next(err);
+		}
 
-    req.tx = tx;
+		req.tx = tx;
 
-    // TODO: Show side chain blocks containing this tx
-    next();
-  });
+		// TODO: Show side chain blocks containing this tx
+		next();
+	});
 });
 
-app.param('addrBase58', function (req, res, next, addr){
-  var pubKeyHash = Util.addressToPubKeyHash(addr);
-  req.pubKeyHash = pubKeyHash;
-  var pubKeyHash64 = pubKeyHash.toString('base64');
+app.param('addrBase58', function (req, res, next, addr)
+{
+	var pubKeyHash = Util.addressToPubKeyHash(addr);
+	req.pubKeyHash = pubKeyHash;
+	var pubKeyHash64 = pubKeyHash.toString('base64');
 
-  rpc.call('explorer.addrquery', [pubKeyHash64], function (err, addr) {
-    if (err) return next(err);
+	rpc.call('explorer.addrquery', [pubKeyHash64], function (err, addr) 
+	{
+		if (err) 
+		{
+			return next(err);
+		}
 
-    req.addr = addr;
+		req.addr = addr;
 
-    next();
-  });
+		next();
+	});
 });
 
 // Routes
-
-app.get('/', function(req, res, next){
-  rpc.call('explorer.indexquery', [10], function (err, result) {
-    if (err) {
-      next(err);
-      return;
-    }
-    result.title = 'Home - Bitcoin Explorer';
-    res.render('index', result);
-  });
+app.get('/', function(req, res, next)
+{
+	rpc.call('explorer.indexquery', [10], function (err, result) 
+	{
+		if (err) 
+		{
+			next(err);
+			return;
+		}
+		result.title = 'Home - Bitcoin Explorer';
+		res.render('index', result);
+	});
 });
 
-app.get('/block/:blockHash', function (req, res) {
-  res.render('block', {
-    title: 'Block '+req.block.block.height+' - Bitcoin Explorer',
-    block: req.block.block,
-    txs: req.block.txs,
-    nextBlock: req.block.nextBlock,
-    totalAmount: req.block.totalAmount,
-    totalFee: req.block.totalFee,
-    totalOut: req.block.totalOut,
-    blockValue: req.block.blockValue,
-    hexDifficulty: bigint(req.block.block.bits).toString(16)
-  });
+app.get('/block/:blockHash', function (req, res) 
+{
+	res.render('block', 
+	{
+		title: 'Block '+req.block.block.height+' - Bitcoin Explorer',
+		block: req.block.block,
+		txs: req.block.txs,
+		nextBlock: req.block.nextBlock,
+		totalAmount: req.block.totalAmount,
+		totalFee: req.block.totalFee,
+		totalOut: req.block.totalOut,
+		blockValue: req.block.blockValue,
+		hexDifficulty: bigint(req.block.block.bits).toString(16)
+	});
 });
 
-app.get('/tx/:txHash', function (req, res) {
-  res.render('transaction', {
-    title: req.tx.tx ?
-      ('Tx '+req.tx.tx.hash.substr(0, 10)+'... - Bitcoin Explorer') :
-      'Unknown tx',
-    tx: req.tx.tx,
-    block: req.tx.block,
-    totalOut: req.tx.totalOut
-  });
+app.get('/tx/:txHash', function (req, res) 
+{
+	res.render('transaction', 
+	{
+		title: ((req.tx.tx) ? ('Tx '+req.tx.tx.hash.substr(0, 10) + '... - Bitcoin Explorer') : 'Unknown tx'), 
+		tx: req.tx.tx, 
+		block: req.tx.block, 
+		totalOut: req.tx.totalOut
+	});
 });
 
-app.get('/address/:addrBase58', function (req, res) {
-  res.render('address', {
-    title: 'Address '+(req.params.addrBase58)+' - Bitcoin Explorer',
-    address: req.params.addrBase58,
-    account: req.addr.account,
-    txs: req.addr.txs
-  });
+app.get('/address/:addrBase58', function (req, res) 
+{
+	res.render('address', 
+	{
+		title: 'Address '+ (req.params.addrBase58) + ' - Bitcoin Explorer',
+		address: req.params.addrBase58,
+		account: req.addr.account,
+		txs: req.addr.txs
+	});
 });
 
 // Only listen on $ node app.js
-
-if (!module.parent) {
-  app.listen(3000);
-  winston.info("Express server listening on port " + app.address().port);
+if (!module.parent) 
+{
+	server.listen(3000);
+	winston.info("Express server listening on port " + server.address().port);
 }
